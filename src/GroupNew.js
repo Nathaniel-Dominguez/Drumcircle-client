72f55bead93bb94062a8b5e1d5ebe499eaf3562c
Nathaniel Dominguez <nathaniel.a.dominguez@gmail.com>
2018-09-17 10:21:39 -0700

    updating new component stubs

---
+++ src/GroupNew.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class GroupNew extends Component {
+	render() {
+		return(
+			<div className="container">
+				<div className="row">
+					<div className></div>
+				</div>
+			</div>
+			)
+	}
+}
