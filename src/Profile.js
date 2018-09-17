841f96d8d4d6354d3a24e6c49562d1e8b33cd033
yuanjli <liyuanjienetwork@gmail.com>
2018-09-17 13:18:22 -0700

    change Group.js and Profile.js

--- src/Profile.js
+++ src/Profile.js
@@ -5,12 +5,30 @@ class Profile extends Component {
     if(this.props.user){
       return (
           <div>
+
             <h2>Hello again, {this.props.user.name}!</h2>
             <h4>Your email is {this.props.user.email}</h4>
+          
+
+
+
+
+                      
+
+
+
+
+
+
+
+
+
+
+
           </div>
         );
     }
-    
+
     return(
       <div>
         <p>This is a profile page. You must be logged in to see it.</p>
