bf647f09129d8d37675c7c45929d6e9595501b12
Nathaniel Dominguez <nathaniel.a.dominguez@gmail.com>
2018-09-17 15:21:21 -0700

    added logo for navbar

--- src/layout/Nav.js
+++ src/layout/Nav.js
@@ -30,6 +30,10 @@ class Nav extends Component {
     return(
         <div>
           <nav className="nav navbar navbar-expand-lg fixed-top navbar-dark bg-primary">
+          <a href="../" class="navbar-brand">
+            <img src="/img/drum_logo.png" className="navbar-logo" />
+            <strong>DrumCircle</strong>
+          </a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
             </button>
