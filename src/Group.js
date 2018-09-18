54439a7d7d628664ece4c5ed8bf2e7705fa5c599
Devan Grose <devangrose@gmail.com>
2018-09-18 12:23:24 -0700

    added components and calls for group feed page

--- src/Group.js
+++ src/Group.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import Post from './Post';
 
 class Group extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      feed: ''
+    }
+  }
   componentDidMount() {
     axios.get(`http://localhost:3000/posts/${this.props.match.params.id}`)
       .then((response) => {
         var posts = response.data;
-        console.log(posts);
+        console.log('posts',posts);
+        var feedNew = posts.map((post) => 
+          <Post content={post}/>
+        );
+        this.setState({feed: feedNew});
       });
   }
 
@@ -16,11 +27,9 @@ class Group extends Component {
 			<div className="container">
 				<div className="row">
 					<div className="col-2">
-            {this.props.match.params.id}
-						<p>Sum stuff</p>
+            {this.state.feed}
 					</div>
 					<div className="col-10">
-						<p>sum Moar Stuff</p>
 					</div>
 				</div>
 			</div>
---
+++ src/Post.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import Comment from './Comment';
+
+class Post extends Component {
+  constructor() {
+    super();
+    this.state = {
+      feed: ''
+    }
+  }
+  componentDidMount(){
+    var comments = axios.get(`http://localhost:3000/comments/5b9ffc9db8357e0f2ddf5e8a`)
+      .then((response) => {
+        var commentFeed = response.data.map((comment) => <Comment content={comment} />);
+        this.setState({ feed: commentFeed});
+      });
+  }
+  render(props){
+    console.log('post',this.props.content._id);
+    var author;
+    if(this.props.content.userId != null){
+      author = this.props.content.userId.name;
+    }else {
+      author = 'Anonymous';
+    }
+    return(
+      <div>
+        <h3>{this.props.content.content}</h3>
+        <p>By: {author}</p>
+        {this.state.feed}
+      </div>
+    );
+  }
+}
+
+export default Post;
+
