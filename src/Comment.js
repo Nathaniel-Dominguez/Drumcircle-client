d22a9eea768fd57e402c1cb3d2de16f7a8f5d0c9
Devan Grose <devangrose@gmail.com>
2018-09-21 08:40:14 -0700

    added delete routes

--- src/Comment.js
+++ src/Comment.js
@@ -4,8 +4,14 @@ import axios from 'axios';
 class Comment extends Component {
   render(props){
     return (
-      <div>
-        <p>{this.props.content.content} -<em>{this.props.content.userId.name}</em></p>
+      <div class="row">
+        <div className="col-md-6">
+          <p>{this.props.content.content} -<em>{this.props.content.userId.name}</em></p>
+        </div>
+        <div className="col-md-6">
+          <button className="btn btn-warning" type="button"> Edit </button>
+          <button className="btn btn-danger"type="button" onClick={(e) => this.props.handleDelete(e,this.props.content._id, this.props.index)}> Delete </button>
+        </div>
       </div>
     )
   }
